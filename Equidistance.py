a59dd4be4f01278dbb89cca2ad2eabac633b8f70
naveenkommineni <56965425+naveenkommineni@users.noreply.github.com>
2020-04-18 14:27:17 +0530

    Update Equidistance.py

--- Equidistance.py
+++ Equidistance.py
@@ -1,5 +1,13 @@
 import sys
-a=sys.argv[1]
-b=sys.argv[2]
-summ=int(a)+int(b)
-print(summ)
+import math
+from math import *
+def Distance():
+  x=int(sys.argv[1])
+  y=int(sys.argv[2])
+  X=pow(x,2)
+  Y=pow(y,2)
+  S=X+Y
+  D=sqrt(S)
+  
+ Distance()
+ 
